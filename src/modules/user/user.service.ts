fd78bd2599dd32da9dfa956eff54ef90dbf8e8b1
artemkons <konstantinov050701@gmail.com>
2022-08-03 18:04:01 +0300

    Add service methods

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,9 +1,41 @@
 import { Injectable, Inject } from '@nestjs/common'
 import { User } from './user.entity'
+import { User as IUser } from './interfaces/User'
 
 @Injectable()
 export class UserService {
   constructor(
     @Inject('UserRepository') private readonly UserRepository: typeof User,
   ) {}
+
+  public async findAll() {
+    return await this.UserRepository.findAll()
+  }
+
+  // FIXME Correctly type options
+  public async findOne(options: any) {
+    return await this.UserRepository.findOne(options)
+  }
+
+  public async findByPk(pk: number) {
+    return await this.UserRepository.findByPk(pk)
+  }
+
+  public async create(user: IUser) {
+    return await this.UserRepository.create(user)
+  }
+
+  public async update(pk: number, newValue: IUser) {
+    const user = await this.UserRepository.findByPk(pk)
+    if (!user) throw new Error('User not fond!')
+
+    return await user.update(newValue)
+  }
+
+  public async destroy(pk: number) {
+    const user = await this.UserRepository.findByPk(pk)
+    if (!user) throw new Error('User not found!')
+
+    return await user.destroy()
+  }
 }
